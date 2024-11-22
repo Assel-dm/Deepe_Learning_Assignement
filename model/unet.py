672f0862ec6f86686028ddcadee4da4e982cc309
Assel-dm <axelfou45@gmail.com>
2024-11-22 17:10:07 +0700

    Update unet.py

--- model/unet.py
+++ model/unet.py
@@ -1,1 +1,13 @@
+#Installation of segmentation-models-pytorch
+pip install segmentation-models-pytorch
+
+from segmentation_models_pytorch import Unet
+
+def build_unet():
+    return Unet(
+        encoder_name="resnet34",
+        encoder_weights="imagenet",
+        in_channels=3,
+        classes=1
+    )
 
