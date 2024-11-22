3c6d103fd904cb5083059a5c20757f4096e512c6
Assel-dm <axelfou45@gmail.com>
2024-11-22 17:08:27 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,33 @@
+#Importation of the library
+import os
+import cv2
+from torch.utils.data import Dataset, DataLoader
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
+#Define the dataset class
+class PolyDataset(Dataset):
+    def __init__(self, image_paths, mask_paths, transform=None):
+        self.image_paths = image_paths
+        self.mask_paths = mask_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image = cv2.imread(self.image_paths[idx])
+        mask = cv2.imread(self.mask_paths[idx], cv2.IMREAD_GRAYSCALE)
+        if self.transform:
+            augmented = self.transform(image=image, mask=mask)
+            image, mask = augmented['image'], augmented['mask']
+        return image, mask
+#Add the augmentations
+transform = A.Compose([
+    A.Resize(256, 256),
+    A.HorizontalFlip(p=0.5),
+    A.RandomBrightnessContrast(p=0.2),
+    A.Rotate(limit=15, p=0.5),
+    A.Normalize(mean=(0.5,), std=(0.5,)),
+    ToTensorV2(),
+])
